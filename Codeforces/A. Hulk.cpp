49055bb86842cd8f831df2ae5eaeb0a0e14ac634
Yossef Hisham <93604359+yossev@users.noreply.github.com>
2024-02-20 20:50:05 +0200

    Create A. Hulk.cpp

---
+++ Codeforces/A. Hulk.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (i == 1) {
+            cout << "I hate ";
+        } else if (i % 2 == 0) {
+            cout << "I love ";
+        } else {
+            cout << "I hate ";
+        }
+
+        if (i < n) {
+            cout << "that ";
+        } else {
+            cout << "it";
+        }
+    }
+
+    return 0;
+}
